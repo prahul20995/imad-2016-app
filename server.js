4f4d68a37cdd4dc1a3dc0d5735a048f6f59b07a4
prahul20995 <prahul20995@gmail.com>
2016-10-04 23:03:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile('article one is executed');
+});
+app.get('/article-two', function (req, res) {
+  res.sendFile("article two is executed");
+});
+app.get('/article-three', function (req, res) {
+  res.sendFile("article three gets executed");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
