3a4b124125bbcd52cc95cbbf4a6bc49b062bddcd
prahul20995 <prahul20995@gmail.com>
2016-10-04 23:27:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('article one is executed');
+  res.send('article one is executed');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile('article two is executed');
+  res.send('article two is executed');
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile('article three gets executed');
+  res.send('article three gets executed');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
